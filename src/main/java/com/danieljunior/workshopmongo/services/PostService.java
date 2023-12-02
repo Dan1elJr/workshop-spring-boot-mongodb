8990d06316c79da35814cd3ad1a16fff0d0e17f4
Daniel Junior <danieljunior1903@gmail.com>
2023-12-02 19:53:07 -0400

    Obtendo post por id

---
+++ src/main/java/com/danieljunior/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.danieljunior.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.danieljunior.workshopmongo.domain.Post;
+import com.danieljunior.workshopmongo.repository.PostRepository;
+import com.danieljunior.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private  PostRepository repo;
+	
+	
+	
+	public Post  findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));		
+			
+	}
+	
+	
+}
