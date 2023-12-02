28b471cbb04c26e5002d71fdff607f2e69bc27cf
Daniel Junior <danieljunior1903@gmail.com>
2023-12-02 16:54:22 -0400

    Projeção dos dados do auto com DTO

--- src/main/java/com/danieljunior/workshopmongo/domain/Post.java
+++ src/main/java/com/danieljunior/workshopmongo/domain/Post.java
@@ -7,20 +7,23 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.danieljunior.workshopmongo.dto.AuthorDTO;
+
 @Document
-public class Post implements Serializable{
-	
+public class Post implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
-	
-	public Post () {}
+	private AuthorDTO author;
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post() {
+	}
+
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -61,15 +64,14 @@ public class Post implements Serializable{
 		this.body = body;
 	}
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
-	
-	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
