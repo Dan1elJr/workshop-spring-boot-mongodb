37db401305518b6141ed71ac487be3e2c641a29f
Daniel Junior <danieljunior1903@gmail.com>
2023-12-02 19:18:58 -0400

    Referenciando os posts do usuário

--- src/main/java/com/danieljunior/workshopmongo/domain/User.java
+++ src/main/java/com/danieljunior/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.danieljunior.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
@@ -16,6 +19,9 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy=true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User () {}
 
@@ -49,7 +55,15 @@ public class User implements Serializable{
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
 
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}	
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -65,5 +79,7 @@ public class User implements Serializable{
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
-	}	
+	}
+
+	
 }
