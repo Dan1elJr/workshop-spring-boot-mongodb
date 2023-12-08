ce9e6b022e778060981f1fb1c7afc36ff1e6c348
Daniel Junior <danieljunior1903@gmail.com>
2023-12-08 19:13:18 -0400

    Consulta simples com query methods

--- src/main/java/com/danieljunior/workshopmongo/services/PostService.java
+++ src/main/java/com/danieljunior/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.danieljunior.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,10 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));		
 			
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 	
 	
